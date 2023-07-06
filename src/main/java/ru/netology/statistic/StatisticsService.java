64dec5f2025327eead2db2141d98bad3037ec2b4
Marakin IQA-65 <le-ha12@yandex.ru>
2023-07-06 20:25:39 +0300

    New pom

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -2,7 +2,6 @@ package ru.netology.statistic;
 
 public class StatisticsService {
     public long findMax(long[] incomes) {
-        long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
             if (current_max < income)
